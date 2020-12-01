6fa71b138fd8a6aa29d565c135118e4973e74a90
Tom Tarpey <tomtarpeydev@gmail.com>
2020-12-01 22:32:03 +0000

    starter code

---
+++ src/demos/demo1.py
@@ -0,0 +1,24 @@
+"""
+You are given a binary tree.
+Write a function that can find the **maximum depth** of the binary tree. The
+maximum depth can be defined as the number of nodes found along the longest
+path from the root down to the furthest leaf node. Remember, a leaf node is a
+node that has no children.
+Example:
+Given the following binary tree
+    5
+   / \
+  12  32
+     /  \
+    8    4
+your function should return the depth = 3.
+"""
+class BinaryTreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def maxDepth(self, root):
+    # Your code here
+    pass
