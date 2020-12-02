cc6146a31990bd9b42e6c9b8c99ef6995aa79e04
sarahmarie1976 <sholle7@gmail.com>
2020-12-02 00:33:19 -0600

    added all four tests

---
+++ Graphs_II/04_csFriendCircles.py
@@ -0,0 +1,35 @@
+"""
+There are N students in a baking class together. Some of them are friends, while some are not friends. The students' friendship can be considered transitive. This means that if Ami is a direct friend of Bill, and Bill is a direct friend of Casey, Ami is an indirect friend of Casey. A friend circle is a group of students who are either direct or indirect friends.
+
+Given a N*N matrix M representing the friend relationships between students in the class. If M[i][j] = 1, then the ith and jth students are direct friends with each other, otherwise not.
+
+You need to write a function that can output the total number of friend circles among all the students.
+
+Example 1:
+
+Input: 
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+Output: 2
+Explanation: The 0th and 1st students are direct friends, so they are in a friend circle. 
+The 2nd student himself is in a friend circle. So return 2.
+Example 2:
+
+Input: 
+[[1,1,0],
+ [1,1,1],
+ [0,1,1]]
+Output: 1
+Explanation: The 0th and 1st students are direct friends, the 1st and 2nd students are direct friends, 
+so the 0th and 2nd students are indirect friends. All of them are in the same friend circle, so return 1.
+[execution time limit] 4 seconds (py3)
+
+[input] array.array.integer friendships
+
+[output] integer
+
+
+"""
+def csFriendCircles(friendships):
+    
---
+++ Tree_Traversal/07_binaryTreeInOrderTraversal.py
@@ -0,0 +1,31 @@
+"""
+
+You are given a binary tree. Write a function that returns the binary tree's node values using an in-order traversal.
+
+Example:
+Input: [2,None,3,4]
+
+   2
+    \
+     3
+    /
+   4
+Output: [2,4,3]
+
+[execution time limit] 4 seconds (py3)
+
+[input] tree.integer root
+
+[output] array.integer
+
+"""
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def binaryTreeInOrderTraversal(root):
+
+
