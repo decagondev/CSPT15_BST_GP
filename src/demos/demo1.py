d93ee101fdb67dc434d2ed6b263264160c75fabf
Tom Tarpey <tomtarpeydev@gmail.com>
2020-12-02 04:37:53 +0000

    end of lecture

--- src/demos/demo1.py
+++ src/demos/demo1.py
@@ -18,7 +18,70 @@ class BinaryTreeNode:
         self.value = value
         self.left = left
         self.right = right
+        
+    # Jason's more OOP method
+    # def maxDepth(self):
+    #    #Base cases
+    #     if self is None:
+    #         return 0 
+    #     if self.left is None and self.right is None: 
+    #         return 1
+    #    #get left height
+    #     if self.left is None: 
+    #         return self.right.maxDepth()+1
+    #    #get right height
+    #     if self.right is None: 
+    #         return self.left.maxDepth() +1
+    #     return max(self.left.maxDepth(), self.right.maxDepth())+1
 
-def maxDepth(self, root):
-    # Your code here
-    pass
+
+    def maxDepth(self, root):
+        # # Your code here O(n)
+        # # base case of empty tree?
+        # if root is None:
+        #     return 0
+
+        # # get left height
+        # left_height = self.maxDepth(root.left)
+
+        # # get right height
+        # right_height = self.maxDepth(root.right)
+
+        # # return the max of left height and right height + 1
+        # return max(left_height, right_height) + 1
+
+        # use a stack for storage O(n)
+        stack = []
+
+        # base case
+        if root is not None:
+            stack.append((1, root))
+
+        # set a depth counter?
+        depth = 0
+
+        # while our stack is not empty?
+        while stack != []:
+            # pop the stack to the current depth and current root node
+            current_depth, root = stack.pop()
+
+            # if our root node is not none
+            if root is not None:
+                # set the depth to the max of depth and current depth
+                depth = max(depth, current_depth)
+                # append the current depth + 1 and the roots left to the stack
+                stack.append((current_depth + 1, root.left))
+                # append the current depth + 1 and the roots right to the stack
+                stack.append((current_depth + 1, root.right))
+
+        # return the depth
+        return depth
+
+
+b1 = BinaryTreeNode(5)
+b1.left = BinaryTreeNode(12)
+b1.right = BinaryTreeNode(32)
+b1.right.left = BinaryTreeNode(8)
+b1.right.right = BinaryTreeNode(4)
+print(b1.maxDepth(b1))
+
--- src/demos/demo2.py
+++ src/demos/demo2.py
@@ -23,6 +23,8 @@ Input:
 Output: False
 Explanation: The root node's value is 10 but its right child's value is 8.
 """
+
+import math # to get the lower and upper bounds math.inf
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, value=0, left=None, right=None):
@@ -30,6 +32,61 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def is_valid_BST(self, root):
-    # Your code here
-    pass
+    # Alilison's recursive solution
+    # def is_valid_BST(self):
+    #         # Your code here
+    #         valid = True
+    #         if self.left:
+    #             valid = valid and self.left.value < self.value
+    #             if not valid:
+    #                 return False
+    #             valid = valid and self.left.is_valid_BST()
+    #         if self.right:
+    #             valid = valid and self.right.value > self.value
+    #             if not valid:
+    #                 return False
+    #             valid = valid and self.right.is_valid_BST()
+    #         return valid
+
+    def is_valid_BST(self, root):
+        # set up a tuple with a stack and the lowest bounds
+        stack, inorder = [], float(-math.inf)
+
+        # while either the stack or the root exists
+        while stack or root:
+            # while the root exists
+            while root:
+                # traverse the left of the tree
+                # append the root to the stack
+                stack.append(root)
+                # increment the root to the left
+                root = root.left
+            
+            # set the root to what is at the top of the stack
+            root = stack.pop()
+
+            # if next element in inorder traversal is smaller than the previous one it is not a bst
+            if root.value <= inorder:
+                # return false
+                return False
+            
+            # set the inorder to the roots value
+            inorder = root.value
+            # set the root to the roots right node
+            root = root.right
+        
+        # return true
+        return True
+
+b1 = TreeNode(5)
+b1.left = TreeNode(3)
+b1.right = TreeNode(7)
+
+b2 = TreeNode(10)
+b2.left = TreeNode(2)
+b2.right = TreeNode(8)
+b2.right.left = TreeNode(6)
+b2.right.right = TreeNode(12)
+
+print(b1.is_valid_BST(b1)) # True
+print(b2.is_valid_BST(b2)) # False
